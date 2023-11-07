6796cbe3588f1e699fa2e36559be8b9d8e8b4889
tireks <33616425+tireks@users.noreply.github.com>
2023-11-07 19:40:56 +0700

    initial

--- app/src/main/java/com/tirexmurina/dishapp/MainActivity.kt
+++ app/src/main/java/com/tirexmurina/dishapp/MainActivity.kt
@@ -16,15 +16,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*DishAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }*/
+            DishAppTheme {
+            }
         }
     }
 }
