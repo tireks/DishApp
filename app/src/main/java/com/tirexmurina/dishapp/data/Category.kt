d4dadb7a39bff36ba3a1a7c66cbdcdab75fc6531
tireks <33616425+tireks@users.noreply.github.com>
2023-11-15 16:41:00 +0700

    data upd

---
+++ app/src/main/java/com/tirexmurina/dishapp/data/Category.kt
@@ -0,0 +1,8 @@
+package com.tirexmurina.dishapp.data
+
+data class Category(
+    val idCategory: String,
+    val strCategory: String,
+    val strCategoryDescription: String,
+    val strCategoryThumb: String
+)
---
+++ app/src/main/java/com/tirexmurina/dishapp/data/CategoryResponse.kt
@@ -0,0 +1,5 @@
+package com.tirexmurina.dishapp.data
+
+data class CategoryResponse(
+    val categories: List<Category>
+)
