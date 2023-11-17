7b2fd77ec36306acfac0dec98a479e1d7f7b1946
tireks <33616425+tireks@users.noreply.github.com>
2023-11-17 15:15:36 +0700

    experiments with preview

--- app/src/main/java/com/tirexmurina/dishapp/screen/CategoryScreen.kt
+++ app/src/main/java/com/tirexmurina/dishapp/screen/CategoryScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,13 +25,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
+import com.tirexmurina.dishapp.R
 import com.tirexmurina.dishapp.data.Category
 import com.tirexmurina.dishapp.presentation.CategoryViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -74,13 +79,13 @@ fun SingleCategoryItem(
                         category.strCategoryThumb
                     ),
                     contentDescription = null,
-                    contentScale = ContentScale.FillBounds,
+                    contentScale = ContentScale.FillHeight,
                     modifier = Modifier.fillMaxSize()
                 )
             }
             Box(modifier = Modifier
                 .background(color = Color.White)
-                .fillMaxSize()
+                .fillMaxWidth()
                 .padding(16.dp)
             ){
                 Text(text = category.strCategory, fontSize = 24.sp)
@@ -88,4 +93,43 @@ fun SingleCategoryItem(
         }
     }
 
+}
+
+@Preview
+@Composable
+fun PreviewItem(){
+    Card(
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ) {
+        Column() {
+            Box(modifier = Modifier.height(230.dp)) {
+                Image(
+                    painterResource(id = R.drawable._09593_1200x600x70_c),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Box(modifier = Modifier
+                .background(color = Color.White)
+                .fillMaxWidth()
+                .padding(16.dp)
+            ){
+                Text(text = "Beer", fontSize = 24.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRecycler(){
+    LazyColumn {
+        items(5) {
+            PreviewItem()
+        }
+    }
 }
